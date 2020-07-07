bbdfa68af3dca1238121077051fd7dadf8887b73
antoniu.pitic <antoniu.pitic@gmail.com>
2020-07-07 10:56:15 +0300

    Versiune 1 - 2 clase - salut in Ro si EN

---
+++ Hello/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mesaje.Afiseaza("Salut lume!", "EN");
+            Mesaje.Afiseaza("Salut lume!", "RO");
+
+        }
+    }
+}
